2e54966ec8f7a733ec47d5b8e4b7a533ac6b065d
dgrace <dormancygrace@gmail.com>
2022-09-28 21:01:29 +0300

    fix output in seminar_2/exercise_2

--- seminar_2/2.py
+++ seminar_2/2.py
@@ -1,18 +1,28 @@
 n = int(input("Введите N: "))
 result = 1
+zero_case = 0
 
 if n < 0:
     for i in range (1, n - 1, -1):
-        if i != 0:
+        if i != 0 and i != n:
             if result < 0:
                 result *= -1
             result *= i
-            print(result)
+            print(result, end = ', ')
+        elif i == n:
+            if result < 0:
+                result *= -1
+            result *= i
+            print(result, end = '.')
         else:
-            result = 0
-            print(result)
-            result = 1
+            result *= i
+            print(result, end = ', ')
+            result += 1
 elif n >= 1:
     for i in range(n):
-        result *= i + 1
-        print(result)
+        if i != n - 1:
+            result *= i + 1
+            print(result, end = ', ')
+        else:
+            result *= i + 1
+            print(result, end = '.')
