72cab7f6e98b0ec0a2fbaf879a3eb1c7fd21a1b7
Gleb Pesin <dormancygrace@gmail.com>
2022-10-11 18:02:45 +0300

    fix exercise_1

--- seminar_4/1.py
+++ seminar_4/1.py
@@ -1,5 +1,8 @@
 import math
 
 d = int(input("Введите желаемое количество знаков после запятой "))
+if d <= 0:
+    print("нет, так дело не пойдет")
+    exit()
 
 print("{:.{}f}".format(math.pi, d))
